4dd43956d2a0c7a027f28b7d831037d48703c672
Douglas Ashton <douglas.j.ashton@gmail.com>
2017-03-26 22:45:59 +0100

    Add main Bathmc controller object

---
+++ src/bathmc.js
@@ -0,0 +1,24 @@
+
+(function(window){
+    'use strict';
+    function Bathmc() {
+        // Core controller
+        // This class creates boxes (and bath's, or is it the bath?) and runs the show
+        var Bathmc = {
+            defaults: {
+              doug: 1,
+              ashton: 2
+            }
+        }
+
+        return Bathmc;
+    };
+
+    //define globally if it doesn't already exist
+    if(typeof(window.Bathmc) === 'undefined'){
+        window.Bathmc = Bathmc();
+    }
+    else{
+        console.log("Bathmc already defined.");
+    }
+})(window);
