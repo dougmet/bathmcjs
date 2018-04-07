cd322d198c8109c5b4116de0add4d017b70ac9d2
Douglas <douglas.j.ashton@gmail.com>
2018-04-07 22:51:28 +0100

    checkpoint potential work

--- src/box.js
+++ src/box.js
@@ -25,6 +25,39 @@ module.exports = function() {
             Utils.assert(this.config.L.length == this.bath.DIM,
                 "bath.DIM must match box dimensions");
         },
+
+        PBCpos: function(posvec) {
+            // For position vectors this applies the periodic boundary conditions
+            for(var k=0;k<posvec.length;k++) {
+                if (!this.wall[k]) { // a wall kills PBCs
+                    if (posvec[k] >= this.L[k])
+                        posvec[k] -= this.L[k];
+                    else if (posvec[k] < 0)
+                        posvec[k] += this.L[k];
+                }
+            }
+            // Unchanged by floppy box
+        },
+        
+        PBCsubtract(vec, plus, minus) {
+            // Same as above but return the magnitude squared
+            var r2=0;
+            for (var k=0;k<vec.length;k++) {
+                vec[k] = plus[k] - minus[k];
+                if (!wall[k]) { // a wall kills PBCs
+                    if (vec[k] > L[k]/2)
+                        vec[k] -= L[k];
+                    else if (vec[k] < -L[k]/2)
+                        vec[k] += L[k];
+                }
+            }
+            
+            for (k=0;k<vec.length;k++)
+                r2 += vec[k]*vec[k];
+            
+            return r2;
+        },
+
         printconfig: function() {console.log(this.config)},
         printme: function() {console.log({'L': this.L, 'wall': this.wall})}
     }
--- src/potential.js
+++ src/potential.js
@@ -2,16 +2,42 @@
 
 module.exports = function() {
   
-	var Utils = require('./utils.js');
+    var Utils = require('./utils.js');
+    var Vec = require('./vector_operations.js');
 
 	var Potential = function(config, bath) {
         
+        this.bath = bath;
+        this.DIM = bath.DIM;
+        this.overlap = false;
+
+        HERE HERE HERE 
 
 		return this;
 	};
 
     Potential.prototype = {
-        printpos: function() {console.log(this.r)}
+        calc_potential: function(p1, p2) {
+            // Select the box we're in
+            var box = p1.box;
+            var dr = Array(this.DIM);	// the vector from p1 to p2
+            s1 = p1.species;
+            s2 = p2.species;
+            
+            double r2 = box.PBCsubtract(dr, p2.r, p1.r); // vec from p1->p2
+            this.overlap=false;
+            
+            if (r2 < this.pot[s1][s2]->cutoff2) {
+                // The general potential call (send in overlap as a reference)
+                // JS TODO - may need to just pass in this instead
+                return(this.pot[s1][s2].pair_pot(this.overlap, r2, dr, p1, p2));
+            }
+            else {
+                // Outside the cutoff, no interaction
+                return(0);
+            }
+        }
+
     }
 
 	return Potential;
